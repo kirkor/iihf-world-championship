7330e6d0c0cb43d93a9484e96af6bba789189d53
KirkoR <grzegorz@bernas.com.pl>
2023-10-26 21:42:22 +0200

    Add score

--- iihf-world-championship/src/main/java/pl/com/bernas/sport/game/Game.java
+++ iihf-world-championship/src/main/java/pl/com/bernas/sport/game/Game.java
@@ -6,8 +6,25 @@ public final class Game {
     public final String homeTeam;
     public final String awayTeam;
 
+    public final Score score = new Score();
+
     public Game(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
+    }
+
+    public class Score {
+        private final int homeTeamScore;
+        private final int awayTeamScore;
+
+        private Score() {
+            this.homeTeamScore = 0;
+            this.awayTeamScore = 0;
+        }
+
+        @Override
+        public String toString() {
+            return homeTeamScore + "-" + awayTeamScore;
+        }
     }
 }
--- iihf-world-championship/src/test/groovy/pl/com/bernas/sport/game/GameTest.groovy
+++ iihf-world-championship/src/test/groovy/pl/com/bernas/sport/game/GameTest.groovy
@@ -3,7 +3,7 @@ package pl.com.bernas.sport.game
 import spock.lang.Specification
 
 class GameTest extends Specification {
-    def 'should be able to create game with two teams'() {
+    def "should be able to create game with two teams"() {
         given:
             String awayTeam = 'Canada'
             String homeTeam = 'USA'
@@ -13,5 +13,25 @@ class GameTest extends Specification {
             hockeyGame.homeTeam == homeTeam
             hockeyGame.awayTeam == awayTeam
 
+    }
+
+    def "each game should start with 0-0 score"() {
+        given:
+            String awayTeam = 'Finland'
+            String homeTeam = 'Sweden'
+        when:
+            Game hockeyGame = new Game(homeTeam, awayTeam)
+        then:
+            hockeyGame.score.toString() == "0-0"
+    }
+
+    def "each game should be showed in format 'home team' 'score' - 'away team' 'score'"() {
+        given:
+            String homeTeam = 'Poland'
+            String awayTeam = 'France'
+        when:
+            Game hockeyGame = new Game(homeTeam, awayTeam)
+        then:
+            hockeyGame.toString() == "${homeTeam} 0 - ${awayTeam} 0"
     }
 }
